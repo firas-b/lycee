4443661578a4b0d60d67f8ec93499368d3481f01
firas-b <boukhchimfiras487@gmail.com>
2023-05-17 19:27:29 +0200

    4th

--- nav.php
+++ nav.php
@@ -1,39 +1,32 @@
-<link rel="stylesheet" href="css/nav.css">
-    <div class="navbar navbar-expand-sm sticky-top  ">
-        <div class="container-fluid justify-content-center px-2">
 
-           
-            <div class="navbar-toggler mx-2 border-0 " data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <i class=" fa-solid fa-bars " style="color: #eff1f0;"></i>
-            </div>
-            <div class="position-relative">
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active text-white" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="man.php">Man</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="woamn.php">Woman</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-white" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item text-white" href="#">Action</a></li>
-                                <li><a class="dropdown-item text-white" href="#">Another action</a></li>
-                                <li>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+<nav class="navbar navbar-expand-lg  p-1"   style="">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
--- utilisateurs/liste_utilisateur.php
+++ utilisateurs/liste_utilisateur.php
@@ -1,74 +1,112 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <title></title>
+<script src="../assets/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../assets/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link rel="stylesheet" href="../css/profile.css">
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-
-<script src="../assets/fontawesome.js"></script>
-</head>
+</head> 
 <body>
-
-<div class="container-fluid ">
-  <div class="row justify-content-center">
-<?php  require '../config.php' ?>
-<table class=" table-sm  w-75 justify-content" style=" overflow:scroll;">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      
-      <th scope="col">nom</th>
-      <th scope="col">prenom</th>
-      <th scope="col">cin</th>
-      <th scope="col"> num-tel</th>
-      <th scope="col"> role</th>
-      <th scope="col"> action</th>
-    </tr>
-  </thead>
-  <tbody class="table-group-divider">
-  
- <?php
-
- try {
-    $query="select * from utilisateur  ";
-    $result = $db->query($query);
-
-     while($row =$result->fetch(PDO::FETCH_ASSOC)){
-        echo '<tr>';
-        echo '<th scope="row">'.$row["id_utilisateur"].'</th>';
-        echo'<td>'.$row['nom'].'</td>';
-        echo'<td>'.$row['prenom'].'</td>';
-        echo '<td>'.$row['cin'].'</td>';
-        echo'<td>'.$row['num_tel'].'</td>';
-        echo'<td>'.$row['role'].'</td>';
-
-        ?>
-        <td><a class="btn btn-sm btn-primary" href="update_utilisateur.php?id=<?=$row['id_utilisateur']?>&role=<?=$row['role']?>">Edit</a>
-  <a href="removeEmployee.php?id=<?= $row['id_utilisateur'] ?>">Delete</a></td>
-        <?php
-     }
-     
-echo'</tbody>
-</table>';
-     $conn = null;
-    }
-    catch(PDOException $e)
-{
-    echo "<br> Probleme de requete".$e->getMessage();
-}
-
- ?>
-  </tbody>
+  <div class="main-content">
+    <!--    naavbar--><link rel="stylesheet" href="../css/nav.css">
+<?php  include '../nav.php'?>
+    <!----->
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="min-height: 600px; background-image: url(https://www.usnews.com/dims4/USNEWS/0db25a9/2147483647/crop/2119x1413+1+0/resize/970x647/quality/85/?url=https%3A%2F%2Fwww.usnews.com%2Fcmsmedia%2F23%2F02%2Fa1a88058409d8bc02755e6ae8bfa%2F200825-studyingbook-stock.jpg); background-size: cover; background-position: center top;">
+      <!-- Mask -->
+      <span class="mask bg-gradient-default opacity-8"></span>
+      <!-- Header container -->
+      <div class="container-fluid d-flex align-items-center">
+        <div class="row">
+          <div class="col-lg-7 col-md-10">
+            <h1 class="display-2 text-white">Hello admin </h1>
+            <p class="text-white mt-0 mb-5">you can add or change the marks of any of your student down below</p>
+            
+            <a href="add_student.html" class="btn btn-info">Ajouter utilisateur</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container-fluid mt--7">
+      <div class="row justify-content-center">
+        
+        <div class="col-xl-10 order-xl-1">
+          <div class="card bg-secondary shadow">
+            <div class="card-header bg-white border-0">
+              <div class="row align-items-center">
+                <div class="col-8">
+                  <h3 class="mb-0">liste des utlisateurs</h3>
+                </div>
+                
+              </div>
+            </div>
+            <div class="card-body">
+              <?php  require '../config.php' ?>
+              <table class=" table-sm  w-100 justify-content" style=" overflow:scroll; border-radious:8px;">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    
+                    <th scope="col">nom</th>
+                    <th scope="col">prenom</th>
+                    <th scope="col">cin</th>
+                    <th scope="col"> num-tel</th>
+                   
+                    <th scope="col"> role</th>
+                    <th scope="col"> action</th>
+                  </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                
+               <?php
+              
+               try {
+                  $query="select * from utilisateur where  role != 'admin'  ";
+                  $result = $db->query($query);
+              
+                   while($row =$result->fetch(PDO::FETCH_ASSOC)){
+                      echo '<tr>';
+                      echo '<th scope="row">'.$row["id_utilisateur"].'</th>';
+                      echo'<td>'.$row['nom'].'</td>';
+                      echo'<td>'.$row['prenom'].'</td>';
+                      echo '<td>'.$row['cin'].'</td>';
+                      echo'<td>'.$row['num_tel'].'</td>';
+                    
+                      echo'<td>'.$row['role'].'</td>';
+              
+                      ?>
+                      <td><a class="btn btn-sm btn-primary" href="update_utilisateur.php?id=<?=$row['id_utilisateur']?>&role=<?=$row['role']?>">editer</a>
+                <a href="removeEmployee.php?id=<?= $row['id_utilisateur'] ?>">supprimer</a></td>
+                      <?php
+                   }
+                   
+              echo'</tbody>
+              </table>';
+                   $conn = null;
+                  }
+                  catch(PDOException $e)
+              {
+                  echo "<br> Probleme de requete".$e->getMessage();
+              }
+              
+               ?>
+                </tbody>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  </div>
-</table>
-
-
+  <footer class="footer">
+    <div class="row align-items-center justify-content-xl-between">
+      <div class="col-xl-6 m-auto text-center">
+        <div class="copyright">
+          <p>Made with <a href="https://www.creative-tim.com/product/argon-dashboard" target="_blank">Argon Dashboard</a> by Creative Tim</p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </body>
 </html>
